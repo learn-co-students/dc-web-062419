b41c1d16713580b8c865a3d16d76b6ed14d3792a
Ann <thuyanannduong@gmail.com>
2019-08-05 10:40:48 -0400

    added 26

---
+++ 26-JS-Intro/script.js
@@ -0,0 +1,32 @@
+//any JS you write here, is going to be loaded in the browser
+
+//
+// let a = {num: 5}
+// let b = {num: 5}
+// console.log("a: ", a)
+// console.log("b: ", b)
+
+//function declaration
+function timesTwo(num){
+  return num * 2
+}
+
+//function expression
+var multiplebyTwo = function(num){
+  return num * 2
+}
+
+//translate between declaration and express
+//remove the function name and set it equal to a variable
+
+//arrow functions
+var multiplebyTwo = x => 2*x
+
+//given a function expression, remove the keyword function
+//add a => after the argument list
+//IF there is one and only one argument, () are optional
+//IF function body is a single line return, remove the return keyword and the {}
+
+
+
+//
