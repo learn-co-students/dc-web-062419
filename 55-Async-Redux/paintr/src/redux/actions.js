f43853ca8141e840b9a4463b0bce831ba66d4b4b
Ann <thuyanannduong@gmail.com>
2019-09-12 09:48:40 -0400

    added redux

---
+++ 55-Async-Redux/paintr/src/redux/actions.js
@@ -0,0 +1,19 @@
+// action creators
+
+function changeSearchText(value) {
+  return { type: "CHANGE_SEARCH_TEXT", value: value };
+}
+
+function vote(paintingId) {
+  return { type: "INCREASE_VOTES", paintingId };
+}
+
+function updatePainting({ title, name, birthday, deathday, paintingId }) {
+  return {
+    type: "UPDATE_PAINTING",
+    payload: { title, name, birthday, deathday },
+    paintingId
+  };
+}
+
+export { changeSearchText, vote, updatePainting };
